25dc6e6a474d96024865fe18f882bd559e9cf1a0
abdelrahman abosamra <web2manag@gmail.com>
2019-03-21 05:18:02 +0200

    PEP8 Reformatting

--- runserver.py
+++ runserver.py
@@ -5,11 +5,11 @@ the server will running on http://localhost:5000/
 import platform
 from os import environ
 from project2 import create_app
-app=create_app()
+app = create_app()
 if __name__ == '__main__':
-    if platform.system() == 'Windows' :
+    if platform.system() == 'Windows':
         HOST = environ.get('SERVER_HOST', 'localhost')
     else:
         HOST = environ.get('SERVER_HOST', '0.0.0.0')
     PORT = 5000
-    app.run(HOST, PORT , debug=True)
+    app.run(HOST, PORT, debug=True)
